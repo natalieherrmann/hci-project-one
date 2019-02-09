510b4c22d34bc3e497319f3a982791d955804472
Natalie Herrmann <nherrma1@hawk.iit.edu>
2019-02-08 18:05:27 -0600

    Add basic disable/enable submit button states

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,6 +7,39 @@
 
   // Run JS once DOM is loaded
   document.addEventListener('DOMContentLoaded', function() {
+
+    var form_submit = document.querySelector('#submit');
+    var name_input = document.querySelector('#name');
+    var email_input = document.querySelector('#email');
+    var name_done = false;
+    var email_done = false;
+
+    // Prohibit users from clicking 'Submit' until they seem to complete the
+    // Name and Email input
+    form_submit.setAttribute('disabled', 'disabled');
+
+    name_input.addEventListener('keyup', function(){
+      var name = this.value;
+      if (name.length >= 1) {
+        name_done = true;
+        doneYet();
+      }
+    });
+
+    email_input.addEventListener('keyup', function(){
+      var email = this.value;
+      if (email.length >= 1) {
+        email_done = true;
+        doneYet();
+      }
+    });
+
+    // Enables the Submit button if both Name and Email are complete
+    function doneYet() {
+      if (name_done && email_done) {
+        form_submit.removeAttribute('disabled');
+      }
+    }
   });
 
 })();
